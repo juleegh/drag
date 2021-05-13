cb5d13b4cf2b42246478557a6788fb1787763380
Juliana Montes <juliana.montes@mojang.com>
2021-05-13 15:49:14 +0200

    Added functionalities to rotate / scale and preview decorations

--- Assets/Scripts/Dress Making/Embelisher.cs
+++ Assets/Scripts/Dress Making/Embelisher.cs
@@ -8,6 +8,10 @@ public class Embelisher : MonoBehaviour
     public static Embelisher Instance { get { return instance; } }
 
     public Color CurrentColor;
+    public Vector3 CurrentScale;
+    public float CurrentRotation;
+
+    private GameObject preview;
 
     void Awake()
     {
@@ -19,6 +23,13 @@ public class Embelisher : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+    }
+
+    void Start()
+    {
+        preview = Inventory.Instance.GetOneDecoration();
+        CurrentScale = Vector3.one;
     }
 
     void Update()
@@ -31,9 +42,42 @@ public class Embelisher : MonoBehaviour
             {
                 GameObject decoration = Inventory.Instance.GetOneDecoration();
                 decoration.transform.position = hit.point;
+                decoration.transform.localScale = CurrentScale;
                 decoration.transform.rotation = Quaternion.LookRotation(hit.normal);
+                decoration.transform.Rotate(Vector3.forward * CurrentRotation, Space.Self);
                 decoration.transform.SetParent(hit.transform);
                 decoration.GetComponent<GarmentDecoration>().SetColor(CurrentColor);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            CurrentRotation -= 2f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            CurrentRotation += 2f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            if (CurrentScale.magnitude < 5)
+                CurrentScale *= 1.1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            if (CurrentScale.magnitude > 1.1f)
+                CurrentScale *= 0.9f;
+        }
+        else
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                preview.transform.position = hit.point;
+                preview.transform.rotation = Quaternion.LookRotation(hit.normal);
+                preview.transform.Rotate(Vector3.forward * CurrentRotation, Space.Self);
+                preview.transform.localScale = CurrentScale;
+                preview.GetComponent<GarmentDecoration>().SetColor(CurrentColor);
             }
         }
     }
