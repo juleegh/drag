66f6350ca0241ee58e12947a294db0d39bb675ed
Juliana Montes <juliana.montes@mojang.com>
2021-07-19 20:00:14 +0200

    Improved UX to toggle erasing button

--- Assets/Scripts/Dress Making/EmbelisherTools.cs
+++ Assets/Scripts/Dress Making/EmbelisherTools.cs
@@ -7,16 +7,37 @@ public class EmbelisherTools : MonoBehaviour
 {
     [SerializeField] private Button drawBtn;
     [SerializeField] private Button eraserBtn;
+    private bool currentErasing;
 
     void Awake()
     {
         drawBtn.onClick.AddListener(() => { ToggleMode(false); });
         eraserBtn.onClick.AddListener(() => { ToggleMode(true); });
         drawBtn.image.color = Color.gray;
+        currentErasing = false;
+    }
+
+    void OnEnable()
+    {
+        if (Embelisher.Instance != null)
+        {
+            ToggleMode(Embelisher.Instance.erasing);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Embelisher.Instance == null)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+            ToggleMode(!currentErasing);
     }
 
     private void ToggleMode(bool erasing)
     {
+        currentErasing = erasing;
         Embelisher.Instance.erasing = erasing;
         drawBtn.image.color = !erasing ? Color.gray : Color.white;
         eraserBtn.image.color = erasing ? Color.gray : Color.white;
